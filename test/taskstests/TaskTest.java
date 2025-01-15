db2f5bbc4529ea52fc7466cd1a3e40932c1167f8
zumikfer <zumikfer21@yandex.ru>
2025-01-15 03:40:14 +0300

    moded

--- test/taskstests/TaskTest.java
+++ test/taskstests/TaskTest.java
@@ -6,13 +6,57 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import status.Status;
+import task.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class TaskTest {
 
+    Task task1;
+
+    @BeforeEach
+    void setUp() {
+        task1 = new Task("name", "description", Status.NEW);
+    }
+
     @Test
-    void shouldReturnTrueIfIdsAreEquals() {
-        Task task1 = new Task("Убраться в комнате", "Протереть пыль, пропылесосить ковер, сделать +" +
-                "влажную уборку", 3, Status.NEW);
-        Task task2 = new Task("Сходить в спортзал", "Сегодня день ног и спины", 3, Status.NEW);
+    void GetterAndSetterForId() {
+        task1.setId(1);
+        assertEquals(1, task1.getId());
+    }
+
+    @Test
+    void GetterAndSetterForName() {
+        task1.setName("new name");
+        assertEquals("new name", task1.getName());
+    }
+
+    @Test
+    void GetterAndSetterForDescription() {
+        task1.setDescription("new description");
+        assertEquals("new description", task1.getDescription());
+    }
+
+    @Test
+    void GetterAndSetterForStatus() {
+        task1.setStatus(Status.DONE);
+        assertEquals(Status.DONE, task1.getStatus());
+    }
+
+    @Test
+    public void EqualsAndHashCode() {
+        Task task1 = new Task("name", "dsc", Status.NEW);
+        Task task2 = new Task("name", "dsc", Status.NEW);
+        task1.setId(1);
+        task2.setId(1);
+
         assertEquals(task1, task2);
+        assertEquals(task1.hashCode(), task2.hashCode());
+
+        task2.setId(2);
+        assertNotEquals(task1, task2);
     }
 }
