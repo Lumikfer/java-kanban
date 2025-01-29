2a6d80741ac77e47adcd89c637ec74c493a58a93
zumikfer <zumikfer21@yandex.ru>
2025-01-29 13:40:27 +0300

    update

--- src/Exception/ManagerLoadFromFileException.java
+++ src/Exception/ManagerLoadFromFileException.java
@@ -6,10 +6,7 @@ import java.util.NoSuchElementException;
 public class ManagerLoadFromFileException extends NoSuchElementException {
     File file;
 
-    public ManagerLoadFromFileException(String message, File file) {
-        super(message);
-        this.file = file;
-    }
+    public ManagerLoadFromFileException(String message, File file) { super(message);  this.file = file;}
 
     public String getDetailMessage() {
         return String.format("%s из файла - %s", getMessage(), file.getAbsolutePath());
