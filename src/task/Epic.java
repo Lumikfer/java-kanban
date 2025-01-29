c372f7bbead8ca63b1674c8ef6d14eed9b65cb3f
zumikfer <zumikfer21@yandex.ru>
2025-01-29 13:52:41 +0300

    update

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -11,7 +11,6 @@ public class Epic extends Task {
     public Epic(String name, String description, Status status, int id) {
         super(name, description, Status.NEW);
 
-
     }
 
     public Epic(String name, String description, int id) {
@@ -24,9 +23,7 @@ public class Epic extends Task {
     public void addSubtask(int subtaskId) {
         if (subtaskId != this.getId()) {
             subtasksId.add(subtaskId);
-
         }
-
     }
 
     public ArrayList<Integer> getSubtasksId() {
@@ -40,14 +37,14 @@ public class Epic extends Task {
     public void removeSubtaskById(int id) {
         subtasksId.remove(Integer.valueOf(id));
     }
-    public static Epic fromString(String value){
-        String [] str = value.split(",");
-        return new Epic(str[2],str[4],Status.valueOf(str[3]),Integer.parseInt(str[0]));
+    public static Epic fromString(String value) {
+        String[] str = value.split(",");
+        return new Epic(str[2], str[4], Status.valueOf(str[3]), Integer.parseInt(str[0]));
     }
 
     @Override
     public String toString() {
-        String line = "" +id+","+type+"," + name+"," + status+"," + description;
+        String line = "" + id + "," + type + "," + name + "," + status + "," + description;
         return line;
     }
 }
