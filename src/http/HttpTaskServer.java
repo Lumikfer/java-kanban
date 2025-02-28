fc4a9ac434ae3d0b226670b51cf54d6142399d2e
zumikfer <zumikfer21@yandex.ru>
2025-02-28 19:06:06 +0300

    update

--- src/http/HttpTaskServer.java
+++ src/http/HttpTaskServer.java
@@ -1,56 +1,51 @@
 package http;
 
 import com.sun.net.httpserver.HttpServer;
-import http.handler.*;
-import http.json.JsonTaskBuilder;
-import managers.TaskManager;
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
-import util.Managers;
-
+import managers.*;
+import tasks.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import com.sun.net.httpserver.HttpServer;
 
 public class HttpTaskServer {
-    private static final int PORT = 8080;
-    private final HttpServer server;
-    private final TaskManager taskManager;
-    private final JsonTaskBuilder jsonTaskBuilder = new JsonTaskBuilder();
 
-    public HttpTaskServer(TaskManager taskManager) throws IOException {
-        this.server = HttpServer.create(new InetSocketAddress(PORT), 0);
-        this.taskManager = taskManager;
+    private static final int PORT = 8080;
+
+    private final HttpServer httpServer;
+
+    public HttpTaskServer(TaskManager taskManager) {
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        httpServer.createContext("/tasks", new TasksHandler(taskManager));
+
+        httpServer.createContext("/epics", new EpicsHandler(taskManager));
+
+        httpServer.createContext("/subtasks", new SubtasksHandler(taskManager));
+
+        httpServer.createContext("/history", new HistoryHandler(taskManager));
+
+        httpServer.createContext("/prioritized", new PrioritizedHandler(taskManager));
     }
 
-    public JsonTaskBuilder getJsonTaskBuilder() {
-        return jsonTaskBuilder;
+    public static void main(String[] args) {
+        TaskManager taskManager = getDefault();
+        HttpTaskServer httpServer = new HttpTaskServer(taskManager);
+        httpServer.start();
     }
 
     public void start() {
-        server.createContext("/tasks", new TaskHandler(taskManager));
-        server.createContext("/subtasks", new SubtaskHandler(taskManager));
-        server.createContext("/epic", new EpicHandler(taskManager));
-        server.createContext("/history", new HistoryHandler(taskManager));
-        server.createContext("/prioritized", new PrioritizedHandler(taskManager));
-        server.start();
+        httpServer.start();
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
     }
 
     public void stop() {
-        server.stop(2);
-    }
-
-    public static void main(String[] args) throws IOException {
-        TaskManager manager = Managers.getDefault();
-        manager.addTask(new Task("dasdasdasd", "asdasdasd", Duration.ofMinutes(100), LocalDateTime.now()));
-        manager.addTask(new Task("dasdasdasd11", "dasdasdasd11", Duration.ofMinutes(100), LocalDateTime.of(2025, 6, 2, 10, 10)));
-        manager.addEpic(new Epic("dasdasdasd22", "dasdasdasd22"));
-        manager.addSubtask(new Subtask("dasdasdasd33", "dasdasdasd33", 3, Duration.ofMinutes(100), LocalDateTime.of(2025, 6, 2, 10, 0)));
-        manager.addSubtask(new Subtask("dasdasdasd212", "dasdasdasd212", 3, Duration.ofMinutes(100), LocalDateTime.of(2025, 7, 2, 10, 0)));
-        HttpTaskServer server = new HttpTaskServer(manager);
-        server.start();
+        httpServer.stop(0);
+        System.out.println("HTTP-сервер остановлен.");
     }
 }
-
