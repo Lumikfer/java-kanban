c3c5d296f4df8a71db406c592ec1fc36d44a1587
zumikfer <zumikfer21@yandex.ru>
2025-03-05 07:37:05 +0300

    update

--- src/http/SubTaskHttpHandler.java
+++ src/http/SubTaskHttpHandler.java
@@ -3,13 +3,15 @@ package http;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import managers.*;
-import  tasks.*;
+import tasks.*;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-public class SubTaskHttpHandler extends  BaseHttpHandler implements HttpHandler {
+public class SubTaskHttpHandler extends BaseHttpHandler implements HttpHandler {
 
-    public  TaskManager taskManager;
+    public TaskManager taskManager;
+
     public SubTaskHttpHandler(TaskManager taskManager) {
         this.taskManager = taskManager;
     }
@@ -57,53 +59,52 @@ public class SubTaskHttpHandler extends  BaseHttpHandler implements HttpHandler 
         return EndPoints.UNKNOWN;
     }
 
-    private void postSubTask(HttpExchange exchange) throws  IOException {
+    private void postSubTask(HttpExchange exchange) throws IOException {
 
-        String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8 );
+        String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
 
         Subtask task = gson.fromJson(requestBody, Subtask.class);
-        if(taskManager.isIntersectionTaskTime(task)) {
+        if (taskManager.isIntersectionTaskTime(task)) {
             sendHasInteractions(exchange);
         }
-        if(taskManager.getTask(task.getId()) == null) {
+        if (taskManager.getTask(task.getId()) == null) {
             taskManager.addSubtask(task);
-            sendText(exchange,"задача добавлена",200);
-        }
-        else {
+            sendText(exchange, "задача добавлена", 200);
+        } else {
             taskManager.updateSubtask(task);
-            sendText(exchange,"задача обновлена",201);
+            sendText(exchange, "задача обновлена", 201);
         }
 
     }
 
-    private  void getSubTask(HttpExchange exchange) throws IOException {
+    private void getSubTask(HttpExchange exchange) throws IOException {
 
         String res = gson.toJson(taskManager.getSubtasks());
-        if(res.isEmpty()) {
-            sendNotFound(exchange,"список пустой");
+        if (res.isEmpty()) {
+            sendNotFound(exchange, "список пустой");
         } else {
-            sendText(exchange,res,200);
+            sendText(exchange, res, 200);
         }
     }
 
     private void getSubTaskId(HttpExchange exchange) throws IOException {
         String[] e = exchange.getRequestURI().getPath().split("/");
         String res = gson.toJson(taskManager.getSubtask(Integer.parseInt(e[2])));
-        if(res.isEmpty()) {
-            sendNotFound(exchange,"такой задачи не суущетсвует");
-        }else{
-            sendText(exchange,res,200);
+        if (res.isEmpty()) {
+            sendNotFound(exchange, "такой задачи не суущетсвует");
+        } else {
+            sendText(exchange, res, 200);
         }
     }
 
     private void delSubTask(HttpExchange exchange) throws IOException {
         String[] e = exchange.getRequestURI().getPath().split("/");
         String res = gson.toJson(taskManager.getSubtask(Integer.parseInt(e[2])));
-        if(res.isEmpty()) {
-            sendNotFound(exchange,"такой задачи не суущетсвует");
-        }else{
+        if (res.isEmpty()) {
+            sendNotFound(exchange, "такой задачи не суущетсвует");
+        } else {
             taskManager.deleteSubtask(Integer.parseInt(e[2]));
-            sendText(exchange,"subtask удален",200);
+            sendText(exchange, "subtask удален", 200);
         }
     }
 }
